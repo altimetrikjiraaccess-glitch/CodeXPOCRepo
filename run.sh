ca3740510578e5fe1b6315a9a008a1aa870f2f6f
Altimetrikjiraaccesstest <altimetrikjiraaccess@gmail.com>
2025-10-06 19:25:20 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 curl -H "Authorization: Bearer <gh_token>" \
      -H "Accept: application/vnd.github+json" \
-     https://api.github.com/repos/<ORG>/<REPO>/dispatches \
-     -d '{"event_type":"codex_generate_tests","client_payload":{"jira_key":"SCRUM-1","mode":"both"}}'
+     https://api.github.com/repos/altimetrikjiraaccess-glitch/CodeXPOCRepo/dispatches \
+     -d '{"event_type":"codex_generate_tests","client_payload":{"jira_key":"SCRUM-1","mode":"jira"}}'
