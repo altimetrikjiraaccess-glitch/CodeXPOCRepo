f50cff3bb8370175642cab6f7bc3db8cc6c243d1
Altimetrikjiraaccesstest <altimetrikjiraaccess@gmail.com>
2025-10-07 20:33:47 +0530

    Source environment before running app

--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# load env vars if available
+if [ -f "envvariable.sh" ]; then
+  # shellcheck disable=SC1091
+  source envvariable.sh
+fi
+
 # compile
 javac JiraACToTestsNoDeps.java
-
 # run
 java JiraACToTestsNoDeps
