2622b6f48e8a5a5c2b6199f88aba7dc05509cdeb
Altimetrikjiraaccesstest <altimetrikjiraaccess@gmail.com>
2025-10-07 15:11:03 +0530

    Update scriptrunner.sh

--- scriptrunner.sh
+++ scriptrunner.sh
@@ -1,5 +1,3 @@
-pip install responses
-
 export JIRA_BASE_URL="https://altimetrikjiraaccess.atlassian.net"
 export JIRA_EMAIL="altimetrikjiraaccess@gmail.com"
 export JIRA_API_TOKEN="Sub_Codex_POC"
@@ -7,5 +5,8 @@ export JIRA_PROJECT_KEY="SCRUM"
 export STORY_KEY="SCRUM-1"
 export TEST_ISSUE_TYPE="Test"           # or "Test Case" (Zephyr), etc.
 export ISSUE_LINK_TYPE="Relates"        # or "Tests", "Blocks", depending on your scheme
+
+pip install responses
+
 
 python3 create_test_from_story.py
