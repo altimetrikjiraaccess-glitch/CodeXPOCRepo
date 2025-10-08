dd1df413931a7656e11b9c1fe099eca833932d14
Altimetrikjiraaccesstest <altimetrikjiraaccess@gmail.com>
2025-10-08 13:25:52 +0530

    Update scriptrunner.sh

--- scriptrunner.sh
+++ scriptrunner.sh
@@ -2,7 +2,7 @@ export NO_PROXY=localhost,127.0.0.1,.altimetrikjiraaccess.atlassian.net
 # Python requests respects these by default
 export JIRA_BASE_URL="https://altimetrikjiraaccess.atlassian.net"
 export JIRA_EMAIL="altimetrikjiraaccess@gmail.com"
-export JIRA_API_TOKEN="Sub_Codex_POC"
+export JIRA_API_TOKEN="ATATT3xFfGF0IxFZXndLW-YYpfvdofTzX-7t4mqpCsJA5Kysaw5eTrJOU5_a2lxFDC8pP0KFagitT5HwNuWzkYaRiEjd44RFgRBt-mxWGyyArw-VxVXd4uLbtIjLNYrUNylCplAkcD83GN-4Lo4ePH2JyU5NWd_-oVi1vwN7MU3k-1wSu-X4tHQ=D0C300D1"
 export JIRA_PROJECT_KEY="SCRUM"
 export STORY_KEY="SCRUM-1"
 export TEST_ISSUE_TYPE="Test"           # or "Test Case" (Zephyr), etc.
